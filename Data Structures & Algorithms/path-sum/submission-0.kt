8e86a80404456d178d84d711ef89f59567c772d0
Paran Soral <paransharma94@gmail.com>
2026-08-25 17:45:01 +0530

    Add: path-sum - submission-0

---
+++ Data Structures & Algorithms/path-sum/submission-0.kt
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
+        if (root == null) return false
+
+        val remaining = targetSum - root.`val`
+        return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining) || 
+        (remaining == 0 && root.left == null && root.right == null)
+    }
+}
