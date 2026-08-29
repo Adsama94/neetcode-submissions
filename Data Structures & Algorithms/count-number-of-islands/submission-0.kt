5ed97e0453b5017bf62e5f10722aa2c3af350405
Paran Soral <paransharma94@gmail.com>
2026-08-29 21:40:34 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.kt
@@ -0,0 +1,31 @@
+class Solution {
+    fun numIslands(grid: Array<CharArray>): Int {
+        if (grid.isEmpty()) {
+            return 0
+        }
+        val numRows = grid.size
+        val numColumns = grid[0].size
+        var numIslands = 0
+        for (i in 0 until numRows) {
+            for (j in 0 until numColumns) {
+                if (grid[i][j] == '1') {
+                    numIslands++
+                    dfsIslands(grid, i, j)
+                }
+            }
+        }
+
+        return numIslands
+    }
+
+    private tailrec fun dfsIslands(grid: Array<CharArray>, i: Int, j: Int) {
+        if (i < 0 || i >= grid.size || j < 0 || j >= grid[0].size || grid[i][j] == '0') {
+            return
+        }
+        grid[i][j] = '0'
+        dfsIslands(grid, i + 1, j)
+        dfsIslands(grid, i - 1, j)
+        dfsIslands(grid, i, j + 1)
+        dfsIslands(grid, i, j - 1)
+    }
+}
